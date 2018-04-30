ea02cce2a5bec1260342249c9bbe98d4ab7971b2
ayesha-x-ansari <33658109+ayesha-x-ansari@users.noreply.github.com>
2018-04-29 22:48:20 -0400

    Update top_movies.rb

--- lib/top_movies.rb
+++ lib/top_movies.rb
@@ -1,5 +1,15 @@
-require "top_movies/version"
+require_relative "./top_movies/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./top_movies/version"
+require_relative './top_movies/movies'
+require_relative './top_movies/scraper'
+require_relative './top_movies/cli'
+
 
 module TopMovies
   # Your code goes here...
 end
+
